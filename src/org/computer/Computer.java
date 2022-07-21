9f96552a583f80e3ffc1cfb10999b4221bc4fb09
ayyappan44 <sjtayyappan@gmail.com>
2022-07-21 13:56:57 +0530

    thanvanth code

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -4,6 +4,9 @@ public class Computer {
 
 	public void computerModel() {
 		System.out.println("Computer Model");
+		System.out.println("model");
+		System.out.println("name");
+		System.out.println("version");
 	}
 	
 	
